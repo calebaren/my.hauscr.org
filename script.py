7570a55f01b879b9311db192774979051970a532
calebren <calebaren@gmail.com>
2019-02-09 19:23:22 -0500

    Update

--- script.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import json
-
-
-SPREADSHEET_QUERY = "https://docs.google.com/spreadsheets/d/12ieimF4voBMBUZ-7y_KP6BHR_O-87cKeAGA0fiLW4m0/gviz/tq?tq=select%20*"
-SPREADSHEET_ID = "12ieimF4voBMBUZ-7y_KP6BHR_O-87cKeAGA0fiLW4m0"
-
-
-def pull_data(spreadsheet_query):
